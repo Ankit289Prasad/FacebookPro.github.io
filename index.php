3cf4d4d26d09a9d4d84955fca487fe471322fa6c
Ankit Prasad <ankit28prasad9@gmail.com>
2019-06-21 13:12:25 +0530

    Progress

--- index.php
+++ index.php
@@ -2,29 +2,39 @@
 
 <head>
     <meta charset="utf-8">
-    <!--meta name="viewport" content="width=device-width, initial-scale=1"-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Facebook Pro-Log in or Sign up</title>
     <link rel="icon" href="pro.ico" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="CSS/main.css">
+    <link rel="stylesheet" type="text/css" href="CSS/m.css">
 </head>
 
 <body>
-    <form action="./Assets/Check.php" method="post"> 
-    <div class="head">
-        <abbr title="A way to connect Peoples"><img src="Assets/Images/facebook.png" /></abbr>
-        <p>Email or Phone &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Password<br />
-            <input type="text" name="userName" />&nbsp&nbsp&nbsp&nbsp&nbsp
-            <input type="password" name="Password" />&nbsp&nbsp&nbsp
-            <input type="submit" value="Log in"/><br/>
-        </p>
-        <a href=""> Forgotten Account?</a>
+    <form action="./Assets/Check.php" method="post">
+        <div class="head">
+            <abbr title="A way to connect Peoples"><img src="Assets/Images/facebook.png" /></abbr>
+            <table style="color:white">
+                <tr>
+                    <td style="height:15px">
+                        <p>Email or Phone
+                    </td>
+                    <td> Password</td>
+                </tr>
+                <tr>
+                    <td style="height:21px"> <input type="text" name="userName" /></td>
+                    <td style="height:21px"><input type="password" name="Password" /></td>
+                    <td style="height:21px"><input type="submit" value="Log in" /></td>
+                </tr>
+                </p>
+            </table>
+            <a href="">&nbsp Forgotten Account?</a>
     </form>
     </div>
     <div class="left">
-
+        <p>Facebook helps you connect and share with the peoples in your life</p>
     </div>
     <div class="right">
 
     </div>
 </body>
+
 </html>
