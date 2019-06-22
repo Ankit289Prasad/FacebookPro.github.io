b0ca0d157212cfb92ea8f4acac131ea7f6de7293
Ankit Prasad <ankit28prasad9@gmail.com>
2019-06-22 13:40:23 +0530

    Progress

--- index.php
+++ index.php
@@ -30,12 +30,104 @@
     </form>
     </div>
     <div class="left">
-        <p><b>Facebook helps you connect and share with the<br/> peoples in your life.</b></p>
-        <img src="Assets/Images/left.png"/>
+        <p><b>Facebook helps you connect and share with the<br /> peoples in your life.</b></p>
+        <img src="Assets/Images/left.png" />
     </div>
     <div class="right">
         <p><b>Create an account</b>
-        <br/>It's free and always will be</p>
+            <br />It's free and always will be.</p>
+        <input type="text" class="first" placeholder="First name">
+        <input type="text" class="first" placeholder="Surname"><br />
+        <input type="text" class="second" placeholder="Mobile number or email address" name="userName"><br />
+        <input type="password" class="second" placeholder="New password" name="Password"><br/><br/>
+        <b>Birthday</b><br/>
+        <select id="date">
+            <option value="01">01</option>
+            <option value="02">02</option>
+            <option value="03">03</option>
+            <option value="04">04</option>
+            <option value="05">05</option>
+            <option value="06">06</option>
+            <option value="07">07</option>
+            <option value="08">08</option>
+            <option value="09">09</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+            <option value="16">16</option>
+            <option value="17">17</option>
+            <option value="18">18</option>
+            <option value="19">19</option>
+            <option value="20">20</option>
+            <option value="21">21</option>
+            <option value="22">22</option>
+            <option value="23">23</option>
+            <option value="24">24</option>
+            <option value="25">25</option>
+            <option value="26">26</option>
+            <option value="27">27</option>
+            <option value="28">28</option>
+            <option value="29">29</option>
+            <option value="30">30</option>
+            <option value="31">31</option>
+        </select>
+        <select id="month">
+            <option value="01">Jan</option>
+            <option value="02">Feb</option>
+            <option value="03">Mar</option>
+            <option value="04">Apr</option>
+            <option value="05">May</option>
+            <option value="06">Jun</option>
+            <option value="07">Jul</option>
+            <option value="08">Aug</option>
+            <option value="09">Sep</option>
+            <option value="10">Oct</option>
+            <option value="11">Nov</option>
+            <option value="12">Dec</option>
+        </select>
+        <select id="year">
+            <option value="1990">1990</option>
+            <option value="1991">1991</option>
+            <option value="1992">1992</option>
+            <option value="1993">1993</option>
+            <option value="1994">1994</option>
+            <option value="1995">1995</option>
+            <option value="1996">1996</option>
+            <option value="1997">1997</option>
+            <option value="1998">1998</option>
+            <option value="1999">1999</option>
+            <option value="2000">2000</option>
+            <option value="2001">2001</option>
+            <option value="2002">2002</option>
+            <option value="2003">2003</option>
+            <option value="2004">2004</option>
+            <option value="2005">2005</option>
+            <option value="2006">2006</option>
+            <option value="2007">2007</option>
+            <option value="2008">2008</option>
+            <option value="2009">2009</option>
+            <option value="2010">2010</option>
+            <option value="2011">2011</option>
+            <option value="2012">2012</option>
+            <option value="2013">2013</option>
+            <option value="2014">2014</option>
+            <option value="2015">2015</option>
+            <option value="2016">2016</option>
+            <option value="2017">2017</option>
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+        </select><br/>
+        <b>Gender</b><br/>
+        <input name="gender" type="radio" value="Male" />Male
+        <input name="gender" type="radio" value="Female"/>Female
+        <input name="gender" type="radio" value="Female"/>Others
+        <p id="term">By clicking Sign Up, you agree to our Terms, Data Policy and Cookie Policy. 
+            You may receive SMS notifications from us and can opt out at any time.</p><br/>
+         <a href="./Assets/Check.php"><input type="button" value="Sign Up"/></a><br/>
+         <p id="footer">&copy; Ankit Prasad : 2019</p>  
     </div>
 </body>
 
