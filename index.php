9624b258f77dcefaded59fbbe3320b48d61d5f5a
Ankit Prasad <ankit28prasad9@gmail.com>
2019-06-22 12:10:13 +0530

    Progress

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Facebook Pro-Log in or Sign up</title>
     <link rel="icon" href="pro.ico" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="CSS/m.css">
+    <link rel="stylesheet" type="text/css" href="CSS/main.css">
 </head>
 
 <body>
     <form action="./Assets/Check.php" method="post">
         <div class="head">
-            <abbr title="A way to connect Peoples"><img src="Assets/Images/facebook.png" /></abbr>
+            <abbr title="A way to connect Peoples"><a href="http://fbprof289.000webhostapp.com/" class="facebook"><img src="Assets/Images/facebook.png" /></a></abbr>
             <table style="color:white">
                 <tr>
                     <td style="height:15px">
@@ -30,10 +30,12 @@
     </form>
     </div>
     <div class="left">
-        <p>Facebook helps you connect and share with the peoples in your life</p>
+        <p><b>Facebook helps you connect and share with the<br/> peoples in your life.</b></p>
+        <img src="Assets/Images/left.png"/>
     </div>
     <div class="right">
-
+        <p><b>Create an account</b>
+        <br/>It's free and always will be</p>
     </div>
 </body>
 
